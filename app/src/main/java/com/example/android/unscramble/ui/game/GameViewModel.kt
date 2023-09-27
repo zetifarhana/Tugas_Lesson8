b6ce6d50c1ed8257d6166258c9ba06dd396115b3
zetifarhana <zetiputrif06@gmail.com>
2023-09-27 15:38:56 +0700

    Mengisi View Model Menambahkan Variabel class baru yang disebut wordlist. Menambahkan Variabel class lain yaitu currentWord Menambahkan metode private getNextWord() untuk mendapatkan kata acak dari allWordList Tetapkan ke val yang disebut tempWord Tambahkan loop while untuk melanjutkan loop hingga kata yang diacak tidak sama dengan kata asli Memanggil metode getNextWord()

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -7,18 +7,42 @@ class GameViewModel : ViewModel() {
 
     private var _score = 0
     private var _currentWordCount = 0
-    private var _currentScrambledWord = "test"
-
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
         init {
             Log.d("GameFragment", "GameViewModel created!")
+            getNextWord()
         }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+    fun nextWord(): Boolean {
+        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 
 }
