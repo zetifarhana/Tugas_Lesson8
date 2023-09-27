21f6ddcb3b90190c7bea5487878c945d5100d05c
firza122 <firzaajikarya112@gmail.com>
2023-09-27 14:09:09 +0700

    memindahkan data currentscrambleword ke viewmodel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,9 +4,13 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    var score = 0
-    private var currentWordCount = 0
-    private var currentScrambledWord = "test"
+    private var _score = 0
+    private var _currentWordCount = 0
+    private var _currentScrambledWord = "test"
+
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
 }
 
 
