ab4867996d8c0b64821a2cb1dc56ad01a99fc320
firza122 <firzaajikarya112@gmail.com>
2023-09-27 14:03:55 +0700

    menambahkan view model di GameFragment

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,7 +3,10 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private val viewModel=GameViewModel()
+
+    var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
 }
 
 
